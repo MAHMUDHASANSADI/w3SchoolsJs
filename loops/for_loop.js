272fe133c5007e42a6508bd0944e5aa5e457a686
Mahmud Hasan <mahmudhasansadi92@gmail.com>
2025-10-07 16:50:09 +0600

    for loop update

---
+++ loops/for_loop.js
@@ -0,0 +1,13 @@
+//take cars as an array
+const cars=['bmw','marcidis','toyota'];
+//take total cars length
+let len = cars.length;
+
+let text = "";
+for(let i = 0;i<len; i++){
+    text = "the car is " + cars[i]+ "\n" ;
+    console.log(text);
+}
+//console.log(text);
+
+
