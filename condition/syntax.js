5c74598477ccd934b1fd00d0f6b2ccacc654e20c
Mahmud Hasan <mahmudhasansadi92@gmail.com>
2025-10-07 14:10:53 +0600

    condition basic

---
+++ condition/syntax.js
@@ -0,0 +1,42 @@
+//only if
+if(condition){
+//code execute if the condition is true
+}
+
+//with else
+
+if(condition){
+//execute if the condition is true
+}
+else{
+//execute if the condition is false
+}
+//else if
+
+if(condition1){
+//execute if the condition1 is true
+}
+else if(coondition){
+//execute if the condition 1 is false and condition is true
+
+}
+else{
+//execute if both condition are false
+}
+
+
+//switch case
+
+let today  = "friday";
+switch(today){
+    case "saturday":
+        console.log('start of the day');
+        break;
+
+    case "friday":
+        console.log("it is holiday");
+        break;
+
+    default:
+        console.log("tata");
+}
